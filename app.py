d4bfa6f40065954001cb3a7544b18f758d7aef5d
harshinireddy04 <160091859+harshinireddy04@users.noreply.github.com>
2026-05-21 14:50:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Databricks Test App",
-    layout="centered"
-)
+st.set_page_config(page_title="Databricks Test")
 
 st.title("Databricks App Working")
 
 st.success("Deployment Successful")
-
-st.write("Streamlit app is running correctly in Databricks Apps.")
